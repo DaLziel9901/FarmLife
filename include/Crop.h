32ab8f02f4a4c4f216af7c28fa98ac08e38d64a4
DaLziel <datday1900@gmail.com>
2025-10-20 11:25:21 +0700

    Add giai đoạn phát triển của nhiều cây

--- include/Crop.h
+++ include/Crop.h
@@ -30,20 +30,14 @@ struct CropData
 // Database plant 
 const std::map<std::string, CropData> CropDatabase = 
 {
-<<<<<<< HEAD
-	{"Carrot", {"Cà rốt",10, 10, 30, 10.0f}},
-	{"Tomato", {"Cà chua",20, 20, 50, 15.0f}},
-	{"Corn", {"Ngô",30, 30, 80, 20.0f}},
-	{"Wheat",{"Lúa mì",6, 8, 20, 8.0f}},
-	{"Strawberry",{"Dâu tây",9,25 ,60 , 18.0f}},
-	{"Cabbage",{"Bắp cải",7, 12, 35, 12.0f}},
-	{"Watermelon",{"Dưa hấu",18, 45, 120, 30.0f}},
-	{"Onion",{"Hành tây",6, 10, 28, 9.0f}},
-=======
-	{"Carrot", {"Cà rốt",10, 10, 30, 5.0f, " Cà rốt", 112, 2}},
-	{"Tomato", {"Cà chua",20, 20, 50, 7.5f, " Cà chua", 144, 2}},
-	{"Corn", {"Ngô",30, 30, 80, 10.0f, " Ngô", 116, 2}}
->>>>>>> temp
+	{"Carrot", {"Cà rốt",10, 10, 30, 5.0f, "Cà rốt", 112, 2}},
+	{"Tomato", {"Cà chua",20, 20, 50, 7.5f, "Cà chua", 144, 2}},
+	{"Corn", {"Ngô",30, 30, 80, 10.0f, "Ngô", 116, 2}},
+	{"Wheat",{"Lúa mì",6, 8, 20, 4.0f, "Lúa mì", 24, 2}},
+	{"Strawberry",{"Dâu tây",9,25 ,60 , 9.0f, "Dâu tây", 80, 1}},
+	{"Cabbage",{"Bắp cải",7, 12, 35, 6.0f, "Bắp cải", 52, 2}},
+	{"Watermelon",{"Dưa hấu",18, 45, 120, 15.0f, "Dưa hấu", 176, 2}},
+	{"Pepple",{"Ớt",6, 10, 28, 4.5f,"Ớt", 152, 2 }},
 };
 
 std::string getCropStageName(CropStage stage);
